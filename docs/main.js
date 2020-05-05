f63035e9c7282512e12c767545efdc193991af2e
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-05-05 23:58:42 +0400

    updated About Me layout, started some Vanilla JS to animate PGP key accordion slide

---
+++ docs/main.js
@@ -0,0 +1,9 @@
+
+/* Accordion slide */
+const slideDown = () => {
+   console.log(event.target)
+}
+
+const pgpKeyContainer = document.querySelector("#pgp-key-container");
+
+pgpKeyContainer.addEventListener("click", slideDown);
