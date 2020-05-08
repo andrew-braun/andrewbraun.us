64c9cf25bc76849cf78f72fc0cc2f354cfcd4d09
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-05-08 23:21:36 +0400

    Animated PGP dropdown toggle

--- docs/main.js
+++ docs/main.js
@@ -1,9 +1,17 @@
 
 /* Accordion slide */
-const slideDown = () => {
-   event.target.style.maxHeight = "100vh";
+const showElement = () => {
+   let element = event.target;
+   let display = element.style.display;
+   element.style.display = display === 'none' ? 'block' : 'none';
+};
+
+const slideDownToggle = () => {
+   let element = event.target;
+
+   element.classList.toggle("slide-toggle");
 }
 
 const pgpKeyContainer = document.querySelector("#pgp-key-container");
+pgpKeyContainer.addEventListener("click", slideDownToggle);
 
-pgpKeyContainer.addEventListener("click", slideDown);
